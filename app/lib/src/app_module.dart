f4ccd26af47a0d1b4676efcaae9075e9b59be65d
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-04-03 02:28:27 -0300

    injection dependence

--- app/lib/src/app_module.dart
+++ app/lib/src/app_module.dart
@@ -1,12 +1,18 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:listinha/src/configuration/domain/services/configuration_service.dart';
+import 'package:listinha/src/configuration/domain/services/configuration_service_imp.dart';
 import 'package:listinha/src/configuration/presenter/ui/pages/configuration_page.dart';
 import 'package:listinha/src/home/core/moduls/home_module.dart';
+import 'package:listinha/src/shared/services/realm/realm_config.dart';
 import 'package:listinha/src/shared/stores/app_store.dart';
+import 'package:realm/realm.dart';
 
 class AppModule extends Module {
   @override
   List<Bind> get binds => [
-        Bind.singleton((i) => AppStore()),
+        Bind.singleton((i) => AppStore(i())),
+        Bind.factory<ConfigurationService>((i) => ConfigurationServiceImp(i())),
+        Bind.instance<Realm>(Realm(config)),
       ];
 
   @override
